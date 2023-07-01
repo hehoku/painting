6686edef0616768b91e375c8a8c61e6683f22051
hehoku <hehoku@outlook.com>
2023-07-01 21:53:29 +0800

    feat: add shape

---
+++ README.md
@@ -0,0 +1,6 @@
+## Painting
+A repository for learning p5.js.
+
+[🔗 https://ipaint.vercel.app/](https://ipaint.vercel.app/)
+
+Refer: https://happycoding.io/tutorials/p5js/
--- src/sketch.js
+++ src/sketch.js
@@ -1,8 +1,13 @@
 function setup() {
-  createCanvas(400, 400)
+  createCanvas(400, 400);
 }
 
 function draw() {
-  background(222)
-  ellipse(200, 200, 100, 100)
+  background(222);
+  fill("#fdfde6");
+  triangle(200, 100, 113.4, 250, 286.6, 250);
+  fill("#008080");
+  ellipse(200, 200, 100, 100);
+  fill("#fdfde6");
+  triangle(156.7, 175, 243.3, 175, 200, 250);
 }
